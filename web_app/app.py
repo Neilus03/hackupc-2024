9796cc6860128c8d546bdd6f6f1fa18bdce95cee
mpilligua <mariaepc31@gmail.com>
2024-05-04 23:30:13 +0200

    update

--- web_app/app.py
+++ web_app/app.py
@@ -43,29 +43,31 @@ def upload_audio():
     if 'audio' in request.files:
         audio = request.files['audio']
         print("Audio File Received:", audio.filename)  # Debugging: log filename
-        try:
+        # try:
             # Define the path to save the audio
-            filename = os.path.join(audio_directory, 'received_audio2.wav')
-            audio.save(filename)
-            
-            print("Audio Saved Successfully in", filename)  # Confirm audio is saved
-            retrieved_images = retrieve(filename, df, kmeans, fclip) # Retrieve images
-            print(retrieved_images.head())
-
-            image_urls = retrieved_images['img_link'].tolist()  # Convert URL column to a list
-            return redirect(url_for('display', image_urls=image_urls))  # Redirect to display images
+        filename = os.path.join(audio_directory, 'received_audio2.wav')
+        audio.save(filename)
         
-        except Exception as e:
-            print("Error processing audio file:", e)  # Log errors
-            return jsonify({"error": "Failed to process the audio file: " + str(e)}), 500
+        print("Audio Saved Successfully in", filename)  # Confirm audio is saved
+        retrieved_images = retrieve(filename, df, kmeans, fclip) # Retrieve images
+        print(retrieved_images.head())
+
+        image_urls = retrieved_images['img_link'].tolist()  # Convert URL column to a list
+        print("Image URLs:", image_urls)  # Log the image URLs
+        return redirect(url_for('display', image_urls=image_urls))  # Redirect to display images
+        
+        # except Exception as e:
+        #     print("Error processing audio file:", e)  # Log errors
+        #     return jsonify({"error": "Failed to process the audio file: " + str(e)}), 500
     else:
         print("No audio file uploaded")  # Log if no file is detected
         return jsonify({"error": "No audio file uploaded"}), 400
 
 @app.route('/display', methods=['GET'])
 def display():
-    image_urls = request.args.get('image_urls').split(',')
+    image_urls = request.args.get('image_urls')
+    print("Displaying images...")  # Log the images to display
     return render_template('display.html', image_urls=image_urls)
 
 if __name__ == '__main__':
-    app.run(debug=False, port=8080)
+    app.run(debug=False, port=8030)
